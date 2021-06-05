87ce2243a79b486b159d53c0e7866cdeb500d425
wilterzan <kabutemerah@gmail.com>
2021-06-05 09:29:23 +0700

    Pohon Adopsi

--- README.md
+++
@@ -1,1 +0,0 @@
-# adopsi.pohonriau
--- js/leaflet-control-geocoder.Geocoder.js
+++ js/leaflet-control-geocoder.Geocoder.js
@@ -10,8 +10,8 @@ module.exports = {
 			collapsed: true,
 			expand: 'touch', // options: touch, click, anythingelse
 			position: 'topright',
-			placeholder: 'Cari tempat...',
-			errorMessage: 'Tempat tidak ditemukan.',
+			placeholder: 'Search...',
+			errorMessage: 'Nothing found.',
 			suggestMinLength: 3,
 			suggestTimeout: 250,
 			defaultMarkGeocode: true
